33f802242332cd9d5889cb6c30f22a62ed531c23
Administrator <Administrator@20190810-105306>
2020-02-09 14:10:54 +0800

    1

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,12 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(2);
-		//3333333
-		//22222222222222222
-		//44444444
-		//11111111111111111111
-		//555555555566666
-		//4564664
+
 
 	}
 }
