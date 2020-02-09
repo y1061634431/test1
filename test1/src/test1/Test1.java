f7fcdc8691caac2ad3114fe876063820a1c9cd4a
y1061634431 <59016095+y1061634431@users.noreply.github.com>
2020-02-09 12:45:11 +0800

    Update Test1.java

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(2);
-		//3333333
+		
 		//22222222222222222
 		//44444444
 		//11111111111111111111
